bc536ed4fe570d54c5e1db7cf76758a4d861a9dc
Gabriele Auriemma <gauriemma@users.noreply.github.com>
2017-03-27 10:11:23 +0200

    Added Google Fonts

--- bootstrap-3.3.7_CDN_minified-starter-template.php
+++ bootstrap-3.3.7_CDN_minified-starter-template.php
@@ -1,19 +1,25 @@
-<!-- Bootstrap 3.3.7 CDN minified starter template -->
+<!-- Bootstrap 3.3.7, FontAwesome, Google fonts CDN minified starter template for prototyping project -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Bootstrap 3.3.7 CDN Boilerplate</title>
-
     <!-- Bootstrap minified CDN CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
     <!-- Bootstrap minified CDN CSS Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
+    <style>
+      h1, h2, h3, h4, h5{
+        font-family: 'Montserrat', sans-serif;
+      }
+      body, p, ul, li{
+        font-family: 'Open Sans', sans-serif;
+      }
+    </style>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
