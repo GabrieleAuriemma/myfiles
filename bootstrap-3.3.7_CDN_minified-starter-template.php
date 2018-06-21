1436606b36ee320d7c50e5dbb45e18b463624a5f
Gabriele Auriemma <13926560+isicreativ@users.noreply.github.com>
2018-06-21 14:59:07 +0200

    Little mistyping correct

--- bootstrap-3.3.7_CDN_minified-starter-template.php
+++ bootstrap-3.3.7_CDN_minified-starter-template.php
@@ -33,7 +33,7 @@
     <!-- jQuery CDN minified (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-    <!-- Bootstrap js CDN minidied -->
+    <!-- Bootstrap js CDN minified -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="https://use.fontawesome.com/c9f9f5af80.js"></script>
   </body>
